1bf2f41ddacd24ffc2e028a9442c2531b81c971b
Kaustav Ghosh <118557832+kaustav812004@users.noreply.github.com>
2025-07-02 18:04:43 +0530

    Add files via upload

---
+++ OnlineStockSpan.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class StockSpanner {
+    stack<pair<int, int>> st; // {price, span}
+    
+public:
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span = 1; 
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+        
+       
+        st.push({price, span});
+        
+        return span;
+    }
+};
+
